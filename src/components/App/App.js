e8e3085862ca5bd7eda810ffd1be5fdb0dcc6d37
charlesmtobias <charlesmtobias@gmail.com>
2023-08-05 10:18:21 -0500

    weighted percentage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,6 +9,7 @@ function App() {
   const [userArtists, setUserArtists] = useState([]);
   const [cancelledArtists, setCancelledArtists] = useState([]);
   const [userCancelled, setUserCancelled] = useState([]);
+  const [percentage, setPercentage] = useState(0);
 
   const handleRangeChange = (event) => {
     setTimeRange(event.target.value);
@@ -25,14 +26,19 @@ function App() {
 
   useEffect(() => {
     let combinedList = [];
-    userArtists.forEach(artist => {
+    let score = 0;
+
+    userArtists.forEach((artist, j) => {
       for(let i = 0; i < cancelledArtists.length; i++) {
           if(cancelledArtists[i].uri === artist.uri) {
             combinedList.push({ ...cancelledArtists[i], src: artist.url, id: artist.id});
+            score += userArtists.length - j;
           }
       }
     })
     setUserCancelled(combinedList);
+    setPercentage(Math.ceil((score / (userArtists.length * (userArtists.length+1)/2)) * 100));
+
   }, [userArtists, cancelledArtists])
 
   const getCancelledArtists = () => {
@@ -43,7 +49,7 @@ function App() {
   return (
     <div className="App">
       <h1>Problematify</h1>
-      <Results artistList={userCancelled} rangeChange={handleRangeChange} />
+      <Results percentage={percentage} artistList={userCancelled} rangeChange={handleRangeChange} />
     </div>
   );
 }
--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -5,13 +5,13 @@ import ArtistList from '../ArtistList/ArtistList'
 const Results = (props) => {
     return (
         <div className="results">
-            <span className="percentage">You have a <span className="green">{props.artistList.length * 2}%</span> problematic rating in the last 
+            <span className="percentage">You have a <span className="green">{props.percentage}%</span> problematic rating in the last 
       <select onChange={props.rangeChange} defaultValue={'medium_term'} >
         <option value="short_term">1 Month</option>
         <option value="medium_term">6 Months</option>
         <option value="long_term">All Time</option>
       </select></span>
-            <div id="bar"><div id="amount" style={{clipPath: `inset(0 ${100-(props.artistList.length * 2)}% 0 0)`}}></div></div>
+            <div id="bar"><div id="amount" style={{clipPath: `inset(0 ${100 - props.percentage}% 0 0)`}}></div></div>
             <ArtistList className="artistList" artists={props.artistList} />
         </div>
     );
